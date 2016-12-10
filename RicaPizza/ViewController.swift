77dd787e8924213cbb6d3fdbc879064b6f9aa70e
Jose Duin <joseduin@joses-mac.local>
2016-12-10 16:23:57 -0400

    Initial Commit

---
+++ RicaPizza/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RicaPizza
+//
+//  Created by Jose Duin on 12/10/16.
+//  Copyright © 2016 Jose Duin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
